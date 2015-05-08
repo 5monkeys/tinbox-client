97c5f9f7f44e877eb4440e19c9e7cd5cb204d49a
Joar Wandborg <joar@wandborg.se>
2015-05-08 12:42:15 +0200

    Packaging fixes

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
-from os import path
+from functools import partial
+import os
 import sys
+
+from os import path
 
 from setuptools import setup, find_packages
 
-from pkg_resources import resource_filename
 
-name = 'tinbox-client'
-package_name = name.replace('-', '_')
+name = 'tinbox-client'  # PyPI name
+package_name = name.replace('-', '_')  # Python module name
+package_path = 'src'  # Where does the package live?
 
 here = path.dirname(path.abspath(__file__))
 
 # Add src dir to path
-src_abs = path.join(here, 'src')
-sys.path.append(src_abs)
+sys.path.append(package_path)
 
 
 # Get the long description from the relevant file
@@ -26,7 +28,7 @@ except Exception:
     pass
 
 
-def get_version():
+def get_version() -> "version string":
     """
     Get the version from a version module inside our package. This is
     necessary since we import our main modules in package/__init__.py,
@@ -37,8 +39,10 @@ def get_version():
     """
     version = {}
 
+    version_file = path.join(package_path, package_name, 'version.py')
+
     # exec the version module
-    with open(resource_filename(package_name, 'version.py')) as fp:
+    with open(version_file) as fp:
         exec(fp.read(), version)
 
     # Call the module function 'get_version'
@@ -50,10 +54,12 @@ setup(
     version=get_version(),
     author='Joar Wandborg',
     author_email='joar@5monkeys.se',
+    url='https://github.com/5monkeys/tinbox-client',
+    license='MIT',
     description='Tinbox client library',
     long_description=long_description,
-    package_dir={'': 'src'},  # Our package root is './src/'.
-    packages=find_packages(exclude=['_*']),
+    package_dir={'': package_path},  # Package lives in ./src
+    packages=find_packages(package_path),
     install_requires=[
         'requests-oauthlib==0.4.2'
     ]
