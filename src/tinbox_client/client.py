cb459c0dfd6137c8ee2e284c4d77a98872a04ccf
adisbladis <adis@blad.is>
2015-08-13 23:07:46 +0800

    Extended client with all API functions

--- src/tinbox_client/client.py
+++ src/tinbox_client/client.py
@@ -34,6 +34,13 @@ class Tinbox:
             'content-type': 'application/json'
         }
 
+    def _get_assignment_path(self, ticket_pk):
+        return '/'.join((
+            'tickets',
+            str(ticket_pk),
+            'assignment',
+            ''))
+
     @with_default_headers
     def post(self, path, *args, **kw):
         return self.session.post(self.get_url(path), *args, **kw)
@@ -44,6 +51,13 @@ class Tinbox:
 
     def get(self, path, *args, **kw):
         return self.session.get(self.get_url(path), *args, **kw)
+
+    def delete(self, path, *args, **kw):
+        return self.session.delete(self.get_url(path), *args, **kw)
+
+    def get_ticket(self, ticket_pk):
+        request = self.get('tickets/{}/'.format(ticket_pk))
+        return request.json()
 
     def create_ticket(self, sender_email, subject, body, sender_name=None,
                       context=None, attachments=None):
@@ -61,6 +75,28 @@ class Tinbox:
 
         return request.json()
 
+    def list_tickets(self, assignee=None, inbox=None, labels=None, pks=True):
+
+        data = {}
+        # List function arguments and iterate
+        for arg in list_tickets.__code__.co_varnames:
+            if arg == 'self':
+                continue
+
+            val = locals()[arg]
+            if val is not None:
+                data[arg] = val
+        request = self.get('tickets/', data=data)
+        return request.json()
+
+    def delete_ticket(self, ticket_pk):
+        return self.delete('tickets/{}/'.format(ticket_pk)).json()
+
+    def get_attachment(self, attachment_pk):
+        return self.get('attachments/{}/'.format(
+            attachment_pk
+        )).json()
+
     def upload_attachment(self, attachment_pk, attachment_bytes):
         return self.put(
             'attachments/{}/'.format(attachment_pk),
@@ -69,3 +105,67 @@ class Tinbox:
                 'content-type': 'application/binary'
             }
         ).json()
+
+    def delete_attachment(self, attachment_pk):
+        request = self.delete('attachments/{}/'.format(
+            attachment_pk
+        ))
+        return request.json()
+
+    def list_attachments(self, message_pk):
+        request = self.get('attachments/',
+                           data={
+                               'message_pk': message_pk
+                           })
+        return request.json()
+
+    def get_message(self, message_pk):
+        request = self.get('messages/{}/'.format(message_pk))
+        return request.json()
+
+    def delete_message(self, message_pk):
+        request = self.delete('messages/{}/'.format(message_pk))
+        return request.json()
+
+    def list_messages(self, ticket_pk):
+        request = self.get('messages/', data={
+            'ticket_pk': ticket_pk
+        })
+        return request.json()
+
+    def create_message(self, thread_id, *args, **kwargs):
+        return self.create_ticket(*args, context=thread_id, **kwargs)
+
+    def get_me(self):
+        request = self.get('me/')
+        return request.json()
+
+    def assign_ticket(self, ticket_pk, assignee_pk):
+        path = self._get_assignment_path(ticket_pk)
+        request = self.put(path, data=json.dumps({
+            'assignee_pk': assignee_pk
+        }))
+        return request.json()
+
+    def unassign_ticket(self, ticket_pk):
+        path = self._get_assignment_path(ticket_pk)
+        request = self.delete(path)
+        return request.json()
+
+    def get_assignment(self, ticket_pk):
+        path = self._get_assignment_path(ticket_pk)
+        request = self.get(path)
+        return request.json()
+
+    def get_desk(self, helpdesk_pk):
+        request = self.get('desks/{}/'.format(helpdesk_pk))
+        return request.json()
+
+    def list_desks(self):
+        return self.get('desks/').json()
+
+    def get_filter(self, filter_pk):
+        return self.get('filters/{}/'.format(filter_pk)).json()
+
+    def list_filters(self):
+        return self.get('filters/').json()
